2106ec2ee111f33cb26ec77078f9eeb0aed16431
MasterEGGway <mastereggway@masterEGGways-MacBook-Air.local>
2023-02-01 19:19:05 -0600

    I defined certain functions and opened the folder

--- a2q1.py
+++ a2q1.py
@@ -3,3 +3,13 @@
    11349288
    Lab Section 08
    cmpt145'''
+
+example = input("Enter file name: ")
+
+def distance(name):
+
+    last = 0
+    steps = []
+    a = open(name, 'r')
+    r = a.read()
+    lines = r.splitlines()
